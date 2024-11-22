515886022da5202a51bf958f21fef51fc1ef92c5
cyberbutterfly3301 <egornostalev@yandex.ru>
2024-11-22 16:29:57 +0500

    add 25 timer realization

--- Timer/MainWindow.axaml.cs
+++ Timer/MainWindow.axaml.cs
@@ -1,12 +1,58 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Threading;
+using System;
+using System.Threading;
+using System.Timers;
+using Tmds.DBus.Protocol;
 
 namespace Timer
 {
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        System.Timers.Timer timer;
+        private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            timer = new System.Timers.Timer(1000);
+            
+            timer.Elapsed += Timer_Elapsed;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+            
+            
+        }
+
+        private void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            Dispatcher.UIThread.Invoke(() => {
+                
+                int minute = int.Parse(Minute.Text);
+                int seconds = int.Parse(Seconds.Text);
+
+                if(seconds == 0)
+                {
+                    if (minute != 0) 
+                    {
+                        Minute.Text = $"{minute-1:D2}";
+                        Seconds.Text = "59";
+                    }
+                    else
+                    {
+                        timer.Stop();
+                    }
+                }
+                if (seconds > 0)
+                {
+                    Seconds.Text = $"{seconds - 1:D2}";
+                }
+            });
         }
     }
 }
